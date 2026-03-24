022d4cf23bbb2aae2a2daa48dd4e8f459daf68f2
fatma-2811 <fatmaalzhraa437@gmail.com>
2026-03-24 12:50:57 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let navbar = document.querySelector(".navbar")
+window.onscroll = function () {
+    if (window.scrollY > 350) {
+        navbar.style.backgroundColor = "#dcc155";
+
+    }
+
+      else {
+    navbar.style.backgroundColor = "white";
+
+}};
+alert('  مرحباً بكم في عيادة جولدن سمايل ')
